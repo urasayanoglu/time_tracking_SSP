87eb50afa38cf809d182bdd48506d17677ee05a6
Jan-Krister Helenius <jan-krister.helenius@edu.turkuamk.fi>
2023-04-16 23:38:37 +0300

    describe structs

--- src/Fileio.h
+++ src/Fileio.h
@@ -11,6 +11,13 @@
 struct User
 {
     unsigned int ID;
+
+    // Using type=0 for the main user, other types may be added if there's time
+    // for subordinate and remote users
+    int type;
+
+    // status = last action
+    int status;
     char firstName[NAME_LENGTH];
     char lastName[NAME_LENGTH];
 };
@@ -18,15 +25,29 @@ struct User
 struct Action
 {
     unsigned int usedID;
+
+    // 0 = Start working, 1 = Stop working
+    // With room to add multiple types of breaks
     int actionType;
+
+    // Dates follow meatspace conventions, here clarified where not immediately apparent
     int year;
     int month;
     int day;
+
+    // Hours since midnight (0-23)
     int hour;
+
+    // Minutes past the hour (0-59)
     int minute;
+
+    // Seconds past the minute (0-59)
     int second;
 };
 
+// Writes binary data to "filename" in the following order:
+// 1. Number of users (int numberOfUsers)
+// 2. Number of actions in the array
 void writeDB(int numberOfUsers, int numberOfActions, char *filename, struct *users, struct *actions);
 
 
