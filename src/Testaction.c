84966bb529695611af98c944a6957b7d86337d16
Jan-Krister Helenius <jan-krister.helenius@edu.turkuamk.fi>
2023-04-17 18:36:26 +0300

    Create action.h stubs

---
+++ src/Testaction.c
@@ -0,0 +1,13 @@
+/*
+ * File:        Testaction.c
+ * Authors:     Uras Ayanoglu, Jan-Krister Helenius, Sebastian Sopola
+ * Description: Unit testing for action.c
+ */
+
+#include "action.h"
+
+int main()
+{
+    // Creating struct arrays for testing
+    struct Action actionNow
+}
---
+++ src/action.h
@@ -0,0 +1,43 @@
+/*
+ * File:        action.h
+ * Authors:     Uras Ayanoglu, Jan-Krister Helenius, Sebastian Sopola
+ * Description: Functions for manipulating the Action struct arrays
+ */
+
+
+#ifndef TIME_TRACKING_SSP_ACTION_H
+#define TIME_TRACKING_SSP_ACTION_H
+
+struct Action
+{
+    unsigned int usedID;
+
+    // 0 = Start working, 1 = Stop working
+    // With room to add multiple types of breaks
+    int actionType;
+
+    // Dates follow meatspace conventions, here clarified where not immediately apparent
+    int year;
+    int month;
+    int day;
+
+    // Hours since midnight (0-23)
+    int hour;
+
+    // Minutes past the hour (0-59)
+    int minute;
+
+    // Seconds past the minute (0-59)
+    int second;
+};
+
+// returns true if the time/date of x is earlier than y
+int isEarlier(struct Action *x, struct Action *y);
+
+// sorts an array of Action structs so that earlier occurrences have smaller index
+void sortActions(struct Action *actions);
+
+// Adds an Action struct to an array of such structs, using current time
+struct Action *addAction(int userID, int actionType, struct Action *actions);
+
+#endif //TIME_TRACKING_SSP_ACTION_H
