0461ea60cb9a4d37f3bada3174cb58292cedfd79
Sebastian <sebastian.sopola@edu.turkuamk.fi>
2023-04-22 17:02:59 +0300

    Ncurse many is up and running

--- src/TimeTrackingMenu.c
+++ src/TimeTrackingMenu.c
@@ -9,6 +9,8 @@
 #include <ncurses.h>
 #include <stdlib.h>
 #include <string.h>
+#include <menu.h>
+
 
 #define CTR_POS(x) ((COLS - strlen(x)) / 2)
 
@@ -20,10 +22,19 @@ char *choices[] = {
     "Exit",
 };
 
-void printMenu()
+// Initialize the highlight to the first choice
+int highlightCurrentOption = 0;
+    
+    
+
+// function to be called when user want to clear menu to original state
+void clearMenu()
 {
+	// Clear the screen
+    clear();
+
 	// Print the title of the menu: (row,built-int print-function(string),string format, the string)
-	char *title = "Time Tracing Menu";
+	char *title = "Time Tracking Menu";
     mvprintw(0, CTR_POS(title), "%s", title);
     
     // Print the selection prompt
@@ -34,64 +45,157 @@ void printMenu()
         mvprintw(4 + index, CTR_POS(choices[index]), "%s", choices[index]);
     }
     
+    // Reset the highlight to the first option
+    highlightCurrentOption = 0;
+    
     // Refresh the screen to show the menu items
     refresh();
 }
 
-int main()
+// function to be called when user navigates in menu and program draws menu again with changes user position
+void printMenu()
 {
-    // Initialize ncurses
-    initscr();
+	// Print the title of the menu: (row,built-int print-function(string),string format, the string)
+	char *title = "Time Tracking Menu";
+    mvprintw(0, CTR_POS(title), "%s", title);
+    
+    // Print the selection prompt
+    mvprintw(2, CTR_POS("Select an option"), "Select an option");
 
-   	/*
-   	RETURN DRAWING PAGE TO HERE IN SAME ORDER AS IT IS IN THE FUNCTION IF NECESSARY
-   	*/
-    
-    // TESTING BELOW ----------------------
-    // Get the number of menu items
-    int numChoices = sizeof(choices) / sizeof(char *);
-    
-    // the key user pressed at any given time, start at top menu item
-    int keyPressed = 0;
-    
-    // Initialize the highlight to the first choice
-    int highlightCurrentOption = 0;
-
-	
-    // Loop until the user selects "Exit"
-    while (1) {
-    
-    	// get user input
-        keyPressed = getch();
-        
-        for (int index = 0; index < 5; index++) {
-            if (index == highlightCurrentOption) {
-                attron(A_REVERSE);
-            }
-            printMenu();
-            attroff(A_REVERSE);
-        }
-        
-        
-        if (keyPressed == KEY_UP) {
-        	// Move selection up
-        	
-    	} else if (keyPressed == KEY_DOWN) {
-        	// Move selection down
-    	}
+	// Print the menu items: (row,built-int print-function(string),string format, the string)
+    for (int index = 0; index < 5; index++) {
+        mvprintw(4 + index, CTR_POS(choices[index]), "%s", choices[index]);
     }
     
-    // TESTING ABOVE -----------------------
+    // Print the menu with highlighted item based on user position in menu
+    for (int index = 0; index < 5; index++) {
+        // Check if the current index is the same as the highlightCurrentOption
+        if (index == highlightCurrentOption) {
+            attron(A_REVERSE); // Turn on reverse video attribute
+        }
+        mvprintw(4 + index, CTR_POS(choices[index]), "%s", choices[index]);
+        if (index == highlightCurrentOption) {
+            attroff(A_REVERSE); // Turn off reverse video attribute
+        }
+    }
     
     
+    // Refresh the screen to show the menu items
+    refresh();
+}
 
-    // Wait for user input
-    getch();
+void initialNcurses() 
+{
+	// Initialize ncurses
+    initscr();
     
-    
+    // Cbreak mode  makes button presses immediately available to the program
+    cbreak();
 
-    // End program
+    // noecho() disables automatic printing of typed characters
+    noecho();
+
+    // sets the stdscr screen up to receive special keys
+    keypad(stdscr, TRUE);
+}
+
+// function to allow move around menu
+void navigateMenu(int highlightCurrentOption)
+{
+    // Calculate the row and column of the current highlighted option
+    int row = 4 + highlightCurrentOption;
+    int col = CTR_POS(choices[highlightCurrentOption]);
+    
+    // Move the cursor to the current highlighted option
+    move(row, col);
+    
+    // Refresh the screen to show the updated cursor position
+    refresh();
+}
+
+// function to close the program
+void terminateProgram()
+{
+    // End ncurses mode
     endwin();
+    
+    // Close program
+    exit(0);
+}
+
+// check user key presses
+void keyPresses() 
+{
+    // Get the number of menu items
+    int numChoices = sizeof(choices) / sizeof(char *);
+
+    // the key user pressed at any given time, start at top menu item
+    int keyPressed = 0;
+
+    // Get user input
+    keyPressed = getch();
+
+    if (keyPressed == KEY_UP) {
+        // Move selection up
+        highlightCurrentOption--;
+        if (highlightCurrentOption < 0) {
+            highlightCurrentOption = 0;
+        }
+    } else if (keyPressed == KEY_DOWN) {
+        // Move selection down
+        highlightCurrentOption++;
+        if (highlightCurrentOption >= numChoices) {
+            highlightCurrentOption = numChoices - 1;
+        }
+    } else if (keyPressed == '5') {
+        // Exit the program when the user selects "Exit"
+        terminateProgram();
+    } else if (keyPressed == '\n' && highlightCurrentOption >= 0 && highlightCurrentOption < numChoices) {
+        // Execute code when user presses enter on the currently highlighted item
+        if (highlightCurrentOption == 0) {
+            // Execute code for "Start day" option
+            // ...
+        } else if (highlightCurrentOption == 1) {
+            // Execute code for "Break" option
+            // ...
+        } else if (highlightCurrentOption == 2) {
+            // Execute code for "End break" option
+            // ...
+        } else if (highlightCurrentOption == 3) {
+            // Execute code for "End day" option
+            // ...
+        } else if (highlightCurrentOption == 4) {
+            // Execute code for "Exit" option
+            terminateProgram();
+        }
+    }
+
+    // Navigate the menu to the current highlighted option
+    navigateMenu(highlightCurrentOption);
+}
+
+// function to run menu 
+void runProgram () 
+{
+	// initialize ncurses, initialize terminal environment, initialize menu
+	initialNcurses();	
+	
+	// infinite loop to run menu
+	while(1)
+	{
+		// Print the menu initially
+		printMenu();
+		
+		// check for user key presses to navigate in the menu
+		keyPresses();
+	}
+}
+
+int main()
+{
+
+	// function to run menu 
+	runProgram();
 
     return 0;
 }
