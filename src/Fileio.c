97a50a8f6724879a649686d8af426a8370ef90d4
Jan-Krister Helenius <jan-krister.helenius@edu.turkuamk.fi>
2023-04-22 23:32:58 +0300

    remove blocks of old code

--- src/Fileio.c
+++ src/Fileio.c
@@ -9,7 +9,7 @@
 #include "action.c"
 
 
-// add user information and actions to database, to 'data.txt' file
+// Save both databases to a file
 int writeDB(int numberOfUsers, int numberOfActions, char *userdataFilename, char *actionFilename, struct User *users,
         struct Action *actions) {
     FILE *fileptr;
@@ -37,52 +37,6 @@ int writeDB(int numberOfUsers, int numberOfActions, char *userdataFilename, char
     {
         return 0;
     }
-
-    /*
-
-    FILE *filePointer = NULL;
-
-    // open the file
-    filePointer = fopen(filename, "a");
-
-    // check was file opened correctly
-    if (filePointer == NULL) {
-        return 0;
-    }
-
-    // loop through all users to add them to file
-    for (int index = 0; index < numberOfUsers; index++) {
-        fprintf(filePointer, "ID: %d, Type: %d, Status: %d, First name: %s Last name: %s\n",
-                (users + index)->ID,
-                (users + index)->type,
-                (users + index)->status,
-                (users + index)->firstName,
-                (users + index)->lastName
-        );
-
-    }
-
-    // loop through all actions to add them to file
-    for (int index = 0; index < numberOfActions; index++) {
-        fprintf(filePointer,
-                "usedID: %d, actionType: %d, Day: %d, Hour: %d, Minutes: %d, Month: %d, Second: %d, Year: %d,\n",
-                (actions + index)->usedID,
-                (actions + index)->actionType,
-                (actions + index)->day,
-                (actions + index)->hour,
-                (actions + index)->minute,
-                (actions + index)->month,
-                (actions + index)->second,
-                (actions + index)->year
-        );
-
-    }
-
-    // close connection to file after information has been saved into it
-    fclose(filePointer);
-
-    return 1;
-    */
 }
 
 // Reads the number of users from the file, then reserves enough memory for
@@ -121,123 +75,18 @@ struct Action *readActionTable(char *filename) {
     struct Action *actionArray = NULL;
     int numberOfActions = 0;
     fileptr = fopen(filename, "r");     // Open in read mode
-    if (fileptr == NULL)
-    {
+    if (fileptr == NULL) {
         return NULL;
-    }
-    else
-    {
+    } else {
         fseek(fileptr, 0, SEEK_SET);
         fread(&numberOfActions, sizeof(int), 1, fileptr);      // Read the integer that tells the length of the array
         actionArray = (struct Action *) malloc(numberOfActions * sizeof(struct Action));
-        if (actionArray == NULL)
-        {
+        if (actionArray == NULL) {
             return NULL;
-        }
-        else
-        {
+        } else {
             fread(actionArray, sizeof(struct Action), numberOfActions, fileptr);     // Read the struct array
         }
         fclose(fileptr);
     }
     return actionArray;
-
-    /*
-    struct User *memoryPointer = NULL;
-
-    memoryPointer = (struct User *) malloc(numberOfUsers * sizeof(struct User));
-
-    // check successfulity of memory allocation
-    if (memoryPointer == NULL) {
-        return NULL;
-    }
-
-    FILE *readFilePointer = NULL;
-
-    readFilePointer = fopen(filename, "r");
-
-    // check was file opened correctly
-    if (readFilePointer == NULL) {
-        return NULL;
-    }
-
-    char line[MAXLINELENGTH];
-    int lineNumber = 0;
-
-    // read from file until we hit empty line
-    while (fgets(line, MAXLINELENGTH, readFilePointer) != NULL) {
-        lineNumber++;
-
-        // check if we are reading the first three lines of the file
-        if (lineNumber >= 1 && lineNumber <= 3) {
-            // assign the user information to the corresponding fields in the User struct
-            sscanf(line, "ID: %u, Type: %d, Status: %d, First name: %s Last name: %s",
-                   &memoryPointer[lineNumber - 1].ID,
-                   &memoryPointer[lineNumber - 1].type,
-                   &memoryPointer[lineNumber - 1].status,
-                   memoryPointer[lineNumber - 1].firstName,
-                   memoryPointer[lineNumber - 1].lastName
-            );
-        }
-    }
-    fclose(readFilePointer);
-    return memoryPointer;
-*/
 }
-
-// MODIFY READ USER TABLE TO WORK WITH WHILE FGETS SO IT READ THE WHOLE FILE ALWAYS !!!!!!!!!!!!!!!
-
-
-/*
-struct Action *readActionTable(int numberOfUsers, int numberOfActions, char *filename) {
-    struct Action *memoryPointer = NULL;
-
-    memoryPointer = (struct Action *) malloc(numberOfActions * sizeof(struct Action));
-
-
-    // check successfulity of memory allocation
-    if (memoryPointer == NULL) {
-        return NULL;
-    }
-
-
-    FILE *readFilePointer = NULL;
-
-    readFilePointer = fopen(filename, "r");
-
-    // check was file opened correctly
-    if (readFilePointer == NULL) {
-        return NULL;
-    }
-
-    char line[MAXLINELENGTH];
-    int lineNumber = 0;
-
-    // read from file until we hit empty line
-    while (fgets(line, MAXLINELENGTH, readFilePointer) != NULL) {
-        lineNumber++;
-
-        // check are we pass user information so we start reading action information
-        if (lineNumber >= numberOfUsers && lineNumber <= (numberOfUsers + numberOfActions)) {
-
-            // assign the action information to the corresponding fields in the Action struct
-            // index determins that action information is added to correct struct Action instance in allocated memory
-            sscanf(line,
-                   "usedID: %d, actionType: %d, Day: %d, Hour: %d, Minutes: %d, Month: %d, Second: %d, Year: %d,",
-                   &memoryPointer[lineNumber - INDEXCORRECTOR].usedID,
-                   &memoryPointer[lineNumber - INDEXCORRECTOR].actionType,
-                   &memoryPointer[lineNumber - INDEXCORRECTOR].day,
-                   &memoryPointer[lineNumber - INDEXCORRECTOR].hour,
-                   &memoryPointer[lineNumber - INDEXCORRECTOR].minute,
-                   &memoryPointer[lineNumber - INDEXCORRECTOR].month,
-                   &memoryPointer[lineNumber - INDEXCORRECTOR].second,
-                   &memoryPointer[lineNumber - INDEXCORRECTOR].year
-            );
-        }
-    }
-    fclose(readFilePointer);
-    return memoryPointer;
-
-}
- */
-
