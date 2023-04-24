210b27dff668785aa5fa3b044e6ce352f165a1c4
Uras Ayanoglu <uras@ayanoglu.org>
2023-04-24 10:38:00 +0300

    Modify comments

--- src/Fileio.h
+++ src/Fileio.h
@@ -55,19 +55,20 @@ struct User
 
 /**
 
-* @brief Writes binary data to "filename" in the following order:
+* @brief Writes binary data to "filename".
+* @details Writes the binary data in the following order:
 * 1. Number of users (int numberOfUsers)
 * 2. Number of actions in the array (int numberOfActions)
 * 3. Array of User structs found at location pointed to by *users
 * 4. Array of Action structs found at location pointed to by *actions
 * The intention is to make this a static storage, loaded upon starting the program,
 * and saved to periodically during runtime, as well as at termination
-* @param numberOfUsers Number of users to be written to file
-* @param numberOfActions Number of actions to be written to file
-* @param userdataFilename Name of the file to which user data will be written
-* @param actionFilename Name of the file to which action data will be written
-* @param users Pointer to an array of User structs
-* @param actions Pointer to an array of Action structs
+* @param[in] numberOfUsers Number of users to be written to file
+* @param[in] numberOfActions Number of actions to be written to file
+* @param[in] userdataFilename Name of the file to which user data will be written
+* @param[in] actionFilename Name of the file to which action data will be written
+* @param[in] users Pointer to an array of User structs
+* @param[in] actions Pointer to an array of Action structs
 * @return 0 for failure, 1 for success
 */
 int writeDB(int numberOfUsers, int numberOfActions, char *userdataFilename, char *actionFilename, struct User *users, struct Action *actions);
@@ -75,8 +76,7 @@ int writeDB(int numberOfUsers, int numberOfActions, char *userdataFilename, char
 
 /**
 
-* @brief Reads the number of users from the file, then reserves enough memory for
-* the user table and reads it. Finally returns the pointer to the array of user structs
+* @brief Reads the number of users from the file, then reserves enough memory for the user table and reads it. 
 * @param filename Name of the file from which user data will be read
 * @return Pointer to an array of User structs. Returns 0 upon failure
 */
@@ -85,8 +85,7 @@ struct User *readUserTable(char *filename);
 
 /**
 
-* @brief Reads the number of actions from the file, then reserves enough memory for
-* the action array and reads it. Finally returns the pointer to the array of action structs
+* @brief Reads the number of actions from the file, then reserves enough memory for the action array and reads it.
 * @param filename Name of the file from which action data will be read
 * @return Pointer to an array of Action structs. Returns NULL upon failure
 */
@@ -95,8 +94,8 @@ struct Action *readActionTable(char *filename);
 /**
 
 * @brief Prompts user for input and saves input in target
-* @param prompt Prompt to be displayed to user
-* @param target Pointer to string where input will be saved
+* @param[in] prompt Prompt to be displayed to user
+* @param[in] target Pointer to string where input will be saved
 */
 void stringInput(char *prompt, char *target);
 
